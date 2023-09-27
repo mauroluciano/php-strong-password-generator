98ef56ee344b5d4f4bd3ec78171e9ff5ef4c5742
mauroluciano <mauroluciano911@gmail.com>
2023-09-27 16:21:56 +0200

    c'è un problema

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+    $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $lunghezza = strlen($caratteri) - 1; 
+
+
+        for ($i = 0; $i < 10; $i++) {
+
+        $random = rand(0, $lunghezza);
+        var_dump ($lunghezza);
+        }
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +21,13 @@
     <title>PHP Password Generator</title>
 </head>
 <body>
-    
+    <form action="GET">
+    <label>Password generata:</label><br>
+    <input type="text" id="password" name="password"><br>
+
+    <button>Genera password</button>
+
+    </form>
+    <?php endif; ?>
 </body>
 </html>
